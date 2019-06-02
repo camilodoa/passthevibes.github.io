7d957a121d80de059e32b9509d68e020bff2a072
camilo <cortiz20@amherst.edu>
2019-06-02 11:20:43 +0100

    edits

---
+++ smooth-scrolling.js
@@ -0,0 +1,14 @@
+// Code found at https://css-tricks.com/snippets/jquery/smooth-scrolling/
+// Scroll to specific values
+  window.scroll({
+    top: 2500,
+    left: 0,
+    behavior: 'smooth'
+  });
+
+  // Scroll certain amounts from current position
+  window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+  });
